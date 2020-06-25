1a987da486d7e0829903b06d425708d72860af2c
mohitshahu108 <mohitshahu12126@gmail.com>
2020-06-25 11:05:39 +0530

    Updates

---
+++ precache-manifest.9e987dc43a6b3eb2520fba2999e886bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6ca3cb5e987d9de1e624213741449e",
+    "url": "/callov/index.html"
+  },
+  {
+    "revision": "880ece7c9c269373cc07",
+    "url": "/callov/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f81faf05b77a9a638c0d",
+    "url": "/callov/static/css/main.e1c0ecca.chunk.css"
+  },
+  {
+    "revision": "880ece7c9c269373cc07",
+    "url": "/callov/static/js/2.25dbe074.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/callov/static/js/2.25dbe074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f81faf05b77a9a638c0d",
+    "url": "/callov/static/js/main.7b67b075.chunk.js"
+  },
+  {
+    "revision": "212c279f1c0d7abec9b2",
+    "url": "/callov/static/js/runtime-main.b8ab5d09.js"
+  },
+  {
+    "revision": "6c1b017f245d63a6eb6abf22c36137fa",
+    "url": "/callov/static/media/img1.6c1b017f.png"
+  }
+]);
